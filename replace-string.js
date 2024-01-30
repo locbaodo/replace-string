f5f5d92b31e9d6d9022933dab6cbb05b5c8a3afe
locbaodo <locbaodo@gmail.com>
2024-01-29 23:02:02 -0800

    Update replace-string.js

--- replace-string.js
+++ replace-string.js
@@ -1,6 +1,6 @@
 let myString = 'Hello World!!!';
 
-// TODO: replace the first occurence of 'o' in `myString` defined above.
+// TODO: replace the first occurence of 'o' with '0' in `myString` defined above.
 // NOTE: DO NOT HARD CODE THE SIZE OF `myString`. When you change the value
 // of `myString`, the test should still pass.
 let newString = undefined;
