45c90c1c00948276f0e959d6d702c5fbe098aaee
meredithGatlin <159324773+meredithGatlin@users.noreply.github.com>
2024-02-07 11:22:25 -0800

    Update replace-string.js

--- replace-string.js
+++ replace-string.js
@@ -3,7 +3,7 @@ let myString = 'Hello World!!!';
 // TODO: replace the first occurence of 'o' in `myString` defined above.
 // NOTE: DO NOT HARD CODE THE SIZE OF `myString`. When you change the value
 // of `myString`, the test should still pass.
-let newString = undefined;
+let newString = myString.replace('o', 'O');
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
 
