9f41fd5dce2d38b980e522f6b85116af78f796d7
hnguyen1537 <159486736+hnguyen1537@users.noreply.github.com>
2024-02-09 14:50:30 -0800

    Update replace-string.js

--- replace-string.js
+++ replace-string.js
@@ -3,7 +3,7 @@ let myString = 'Hello World!!!';
 // TODO: replace the first occurence of 'o' in `myString` defined above.
 // NOTE: DO NOT HARD CODE THE SIZE OF `myString`. When you change the value
 // of `myString`, the test should still pass.
-//let newString = undefined;
+// let newString = undefined;
 
 let newString = myString.replace("Hello World!!!", "Hell0 World!!!");
 
